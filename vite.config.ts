b8fcebb91a92c8248e6da7e304be4e60902ffb3e
Rbbb <zero102x@gmail.com>
2025-10-28 17:36:07 +0800

    ✨ feat: Update Vite configuration to dynamically set base URL based on environment mode; replace old manifest file with a new versioned manifest for routing assets.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  console.info('Vite mode:', mode);
+  console.info('Vite base url:', process.env.VITE_BASE_URL);
+  return {
+    base: mode === 'production' ? process.env.VITE_BASE_URL : '/',
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  };
 });
